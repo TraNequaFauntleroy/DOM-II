b5c6ac8fcca7bc26d515d147e21cdf2ae7732283
NeNeTheCreative <shecodes@nenethecreative.com>
2021-08-03 18:12:19 -0400

    created all query selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+const headLine = document.querySelector('.logo-heading');
+console.log('headline:', headLine);
+const navHeader = document.querySelector('.main-navigation');
+console.log('nav header:', navHeader);
+const navItems = document.querySelectorAll('.nav-link');
+console.log('nav items:', navItems);
+const yellowBus = document.querySelector('.intro img');
+console.log('yellow bus:', yellowBus);
+const travelPics = document.querySelectorAll('.img-content');
+console.log('travel pics:', travelPics);
+const boatPic = document.querySelector('.content-destination img');
+console.log('boat pic:',boatPic);
+// const buttons = document.querySelectorAll('.destination btn');
+// console.log('buttons:', buttons);
+const copyRight = document.querySelector('.footer');
+console.log('copyright:', copyRight);
+
+function mouseOver(event){
+    if (event.type === 'mouseover') {
+        event.target.style.backgroundColor = "red";
+    } else if (event.type === 'mouseup') {
+        event.target.style.backgroundColor = "white";
+    }
+};
+
+headLine.addEventListener('mouseover', mouseOver);
+
+
