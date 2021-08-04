5486719eb91da8235d4824e6725884b4145a89cb
NeNeTheCreative <shecodes@nenethecreative.com>
2021-08-04 09:54:31 -0400

    8 event completed

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
 
 //  QUERY SELECTORS
-const headLine = document.querySelector('.logo-heading');
+const headLine = document.querySelector('.logo-heading'); 
 console.log('headline:', headLine);
-const navHeader = document.querySelector('.main-navigation');
-console.log('nav header:', navHeader);
-const navItems = document.querySelectorAll('.nav-link');
-console.log('nav items:', navItems);
 const yellowBus = document.querySelector('.intro img');
 console.log('yellow bus:', yellowBus);
-const wordDiv = document.querySelectorAll('.content-section');
-console.log('word div:', wordDiv);
 const boatPic = document.querySelector('.content-destination img');
 console.log('boat pic:',boatPic);
 const buttons = document.querySelectorAll('.btn');
@@ -29,37 +23,56 @@ function mouseOut(event){
 };
 headLine.addEventListener('mouseout', mouseOut);
 
-// KEYDOWN EVENT
-function escapeKey(event){
-   alert(event.target.style.border = '3px solid pink');
-    };
-yellowBus.addEventListener('keydown', escapeKey);
-
 // ONCLICK EVENT
 function clickIt(event){
     window.location = 'https://www.buses.com/'
 };
-buttons.addEventListener('click', clickIt);
-
+buttons.forEach(function(item){
+    item.addEventListener('click', clickIt);
+});
 
 //DOUBLE CLICK EVENT 
 function dblClick(event){
-    window.location = 'https://www.instagram.com/nenethecreative/'
-}
-boatPic.addEventListener('dblclick', dblClick);
-
-// SCROLL EVENT
-function scrollIt(event) {
-    event.target.style.border = '1px solid black';
-    event.target.style.width = '200px';
-    event.target.style.height = '300px';
-    event.target.style.overflow = 'scroll';
-}
-wordDiv.addEventListener('scroll', scrollIt);
-
-// SELECT EVENT
-function selectIt(event){
     event.target.style.backgroundColor = 'green';
-    event.target.style.textColor = 'white';
+}
+copyRight.addEventListener('dblclick', dblClick);
+
+// KEYDOWN EVENT
+function tKey(event){
+    event.target.textContent += `${event.code}`;
 };
-copyRight.addEventListener('select', selectIt);
+document.addEventListener('keydown', tKey);
+
+// WHEEL 
+function zoom(event) {
+    let scale = 1;
+    event.preventDefault();
+if (event.deltaY < 0) {
+  // Zoom in
+  scale *= event.deltaY * -2;
+}
+else {
+  // Zoom out
+  scale /= event.deltaY * 2;
+}
+// Apply scale transform
+event.target.style.transform = `scale(${scale})`;
+};
+yellowBus.addEventListener('wheel', zoom);
+
+// MOUSEENTER EVENT
+function mouseEnter(event) {
+    event.target.style.border = '10px hotpink dotted';
+}
+boatPic.addEventListener('mouseenter', mouseEnter);
+
+// MOUSELEAVE EVENT
+function mouseLeave(event){
+    event.target.style.border = '0px white dotted';
+}
+boatPic.addEventListener('mouseleave', mouseLeave);
+
+
+
+
+
